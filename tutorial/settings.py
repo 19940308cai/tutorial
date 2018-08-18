9c1fc7b93922f13556f17b67f7404c61f6b40418
caijiang <q152625424@qq.com>
2018-08-18 17:45:47 +0800

    fix

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -21,7 +21,7 @@ USER_AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (
 
 # Obey robots.txt rules
 # ROBOTSTXT_OBEY = False
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
